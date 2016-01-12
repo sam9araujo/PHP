c31ecdff51fb207e1af6f9dc900a88ea39fe0e92
Samanta Araujo <samanta@sam9araujo.com>
2016-01-12 14:03:27 -0200

    Form email
    Just host Linux

---
+++ form_send_email/page.php
@@ -0,0 +1,33 @@
+﻿<?php
+if (isset($_GET['success'])) {
+    echo 'Thanks! :)';
+}
+?>
+<!DOCTYPE html>
+<html lang="en" xmlns="http://www.w3.org/1999/html">
+<head>
+    <meta charset="iso-8859-1">
+    <title>Web Site [Name Company]</title>
+    </script>
+</head>
+<body>	
+
+<form action="send.php" method="post">
+<label>Name:</label><br/>
+<input name="name" type="text" class="form-control" id="name">
+<br/>
+<label>Phone:</label><br/>
+<input name="phone" type="number" class="form-control" id="phone">
+<br/>
+<label>Email:</label><br/>
+<input name="email" type="email" class="form-control" id="email">
+<br/>
+<label>Message:</label><br/>
+<textarea name="message" rows="4" class="form-control" id="message"></textarea>
+<br/>
+<input type="submit" value="Send" class="form-control">
+
+</form>
+
+</body>
+</html>
---
+++ form_send_email/send.php
@@ -0,0 +1,52 @@
+﻿<?php
+
+$name      = $_POST['name'];
+$emailFrom = $_POST['email'];
+$phone  = $_POST['phone'];
+$message  = $_POST['message'];
+$emailEnd  = "samanta@sam9araujo.com";
+$head      = "Form - Web Site [Name Company] - Client: ".$name;
+
+require("phpmailer/class.phpmailer.php");
+
+$mail = new PHPMailer();
+
+
+$mail->IsSMTP(); 
+$mail->Host = "smtp.sam9araujo.com"; // SMTP (useu o host smtp.yourdomain.com)
+$mail->SMTPAuth = true; // required to smtp.yourdomain.com
+$mail->SMTPSecure = '';
+$mail->Port = 587;
+$mail->SMTPDebug = 1;
+$mail->Username = $emailEnd; // SMTP (adress email)
+$mail->Password = 'abfs2015'; // Password host SMTP (password email used)
+
+$mail->From = $emailFrom;
+$mail->Sender = $emailEnd; 
+$mail->FromName = $name; 
+
+$mail->AddAddress($emailEnd,$head);
+
+$mail->IsHTML(true);
+$mail->CharSet = 'utf-8'; // Charset  message (optional)
+
+$mail->Subject  = $head; // subject message
+$mail->Body = $name.'<br><p>Phone: '.$phone.'<br/>Email: '.$emailFrom.'<br/>'.$message.'</p>';
+$mail->AltBody = $name.'<br><p>Phone: '.$phone.'<br/>'.$message.'</p>';
+
+
+// Send  email
+$send = $mail->Send();
+
+// clean
+$mail->ClearAllRecipients();
+$mail->ClearAttachments();
+
+// Show message
+if ($send) {
+    echo "<script>location.href='page.php?success=yes';</script>";
+} else {
+    echo "<script>location.href='page.php'; alert('Info error:' . $mail->ErrorInfo)</script>";
+}
+
+?>
